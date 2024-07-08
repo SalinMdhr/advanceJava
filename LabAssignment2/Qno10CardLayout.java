de994d2163c5d0c706a6da71c2cc536bedd9f1de
SalinMdhr <salin.manandhar@gmail.com>
2024-07-08 21:59:26 +0545

    Third Commit

--- LabAssignment2/Qno10CardLayout.java
+++ LabAssignment2/Qno10CardLayout.java
@@ -1,35 +1,35 @@
 package LabAssignment2;
 
 import java.awt.CardLayout;
-
 import javax.swing.*;
 
 public class Qno10CardLayout {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Salin");
+        JPanel mainPanel = new JPanel();
         JPanel panel1 = new JPanel();
         JPanel panel2 = new JPanel();
         JButton btn1 = new JButton("Button 1");
         JButton btn2 = new JButton("Button 2");
 
         CardLayout cl = new CardLayout();
-        panel1.setLayout(cl);
+        mainPanel.setLayout(cl);
         
         btn1.addActionListener(e -> {
-            cl.show(panel1, "2");
+            cl.show(mainPanel, "2");
         });
 
         btn2.addActionListener(e -> {
-            cl.show(panel1, "1");
+            cl.show(mainPanel, "1");
         });
 
         panel1.add(btn1);
         panel2.add(btn2);
-
-        panel1.add(panel2, "2");
-        cl.show(panel1, "1");
         
-        frame.add(panel1);
+        mainPanel.add(panel1, "1");
+        mainPanel.add(panel2, "2");
+        
+        frame.add(mainPanel);
         frame.setSize(200, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
