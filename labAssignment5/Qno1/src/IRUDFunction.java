5ffb1d259d530c8561a0288ff5a84377a6575206
SalinMdhr <salin.manandhar@gmail.com>
2024-07-16 22:20:54 +0545

    new commit

--- labAssignment5/Qno1/src/IRUDFunction.java
+++ labAssignment5/Qno1/src/IRUDFunction.java
@@ -1,6 +1,6 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+// import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class IRUDFunction {
     public void insertFunction(
@@ -25,4 +25,50 @@ public class IRUDFunction {
             System.out.println(ex.getMessage());
         }
     }
+
+    public void retrieveFunction(DefaultTableModel model) {
+        String query = "SELECT * FROM students";
+        try(
+            Connection conn = JDBCConnection.getConn();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query)
+        ) {
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("fname");
+                String address = rs.getString("address");
+                String gender = rs.getString("gender");
+                String dob = rs.getString("DOB");
+                String program = rs.getString("program");
+                String contact = rs.getString("contact");
+                String email = rs.getString("email");
+                model.addRow(new Object[]{id, name, address, gender, dob, program, contact, email});
+            }
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+        }
+    }
+
+    public void updateFunction(
+        int id, String name, String address, String dob, String gender, String program, String phone, String email
+    ) {
+        String query = "UPDATE students SET fname = ?, address = ?, gender = ?, DOB = ?, program = ?, contact = ?, email = ?";
+        try(
+            Connection conn = JDBCConnection.getConn();
+            PreparedStatement pstmt = conn.prepareStatement(query)
+        ) {
+            pstmt.setString(1, name);
+            pstmt.setString(2, address);
+            pstmt.setString(3, gender);
+            pstmt.setString(4, dob);
+            pstmt.setString(5, program);
+            pstmt.setString(6, phone);
+            pstmt.setString(7, email);
+            pstmt.executeUpdate();
+            System.out.println("MMMM");
+        } catch (Exception e) {
+            System.out.println("IRUD: ERROR" + e.getMessage());
+        }
+    }
+
 }
---
+++ labAssignment5/Qno1/src/ListStudent.java
@@ -0,0 +1,78 @@
+import java.awt.Dimension;
+// import java.sql.SQLException;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+
+public class ListStudent {
+    private static DefaultTableModel model;
+    public ListStudent() {
+        JFrame frame = new JFrame("Student List");
+        JPanel panel = new JPanel();
+
+        JButton updateBtn = new JButton("Update");
+
+        JTable table = new JTable();
+        table.setPreferredScrollableViewportSize(new Dimension(800, 100));
+        model = new DefaultTableModel();
+        table.setModel(model);
+        model.addColumn("Id");
+        model.addColumn("Name");
+        model.addColumn("Address");
+        model.addColumn("Gender");
+        model.addColumn("Date of Birth");
+        model.addColumn("Program");
+        model.addColumn("Contact");
+        model.addColumn("Email");
+
+        TableColumn emailColumn = table.getColumnModel().getColumn(7); // Index 3 for the "Email" column
+        emailColumn.setPreferredWidth(200); // Set preferred width as 200 pixels
+
+        IRUDFunction retrive = new IRUDFunction();
+        retrive.retrieveFunction(model);
+
+        JScrollPane sp = new JScrollPane(table);
+        sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        updateBtn.addActionListener(e -> updateStudentDetails(table));
+
+        panel.add(sp);
+        panel.add(updateBtn);
+        
+        frame.add(panel);
+        frame.setSize(200, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new ListStudent();
+    }
+
+    private static void updateStudentDetails(JTable table) {
+        int selectedRow = table.getSelectedRow();
+        if (selectedRow == -1) {
+            System.out.println("NO ROW SELECTED");
+        }
+
+        int id = (int) model.getValueAt(selectedRow, 0);
+        String name = (String) model.getValueAt(selectedRow, 1);
+        String address = (String) model.getValueAt(selectedRow, 2);
+        String gender = (String) model.getValueAt(selectedRow, 3);
+        String dob = (String) model.getValueAt(selectedRow, 4);
+        String program = (String) model.getValueAt(selectedRow, 5);
+        String contact = (String) model.getValueAt(selectedRow, 6);
+        String email = (String) model.getValueAt(selectedRow, 7);
+
+        try {
+            IRUDFunction update = new IRUDFunction();
+            update.updateFunction(id, name, address, dob, gender, program, contact, email);
+
+            System.out.println("UPDATED");
+        } catch (Exception ex) {
+            System.out.println("ERROR" + ex.getMessage());
+
+        }
+
+    }
+
+}
